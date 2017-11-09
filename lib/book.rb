b3305db19202ed92afa333d4e4e7fe8a09437d26
Nolan Byrnes <nolanehs@gmail.com>
2017-11-09 12:03:13 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
 
 GENRES = []
@@ -11,5 +11,7 @@ GENRES = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+
 
 end
